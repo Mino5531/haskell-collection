5c749923ac22e139c08a7be4e37d18442ac1c48e
Mino5531 <paffrath@yangnet.de>
2020-10-25 23:47:18 +0100

    Update hanoi.hs

--- hanoi.hs
+++ hanoi.hs
@@ -1,5 +1,4 @@
 hanoiLoesen :: Int -> String -> String -> String -> [(String,String)]
-hanoiLoesen n a b c = hanoiZuListe n a b c []
 
-hanoiZuListe 0 _ _ _ l = l
-hanoiZuListe n a b c l = hanoiZuListe(n-1) a c b ((a,b) : hanoiZuListe (n-1) c b a l)
+hanoiLoesen 0 _ _ _ = []
+hanoiLoesen n a b c = hanoiLoesen (n-1) a c b ++ [(a,b)] ++ hanoiLoesen (n-1) c b a
