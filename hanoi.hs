fc8215ec503228455fbf5f1b395dda1ee4baea28
Mino5531 <paffrath@yangnet.de>
2020-10-26 11:22:00 +0100

    Changed parameter names

--- hanoi.hs
+++ hanoi.hs
@@ -1,4 +1,4 @@
-hanoiLoesen :: Int -> String -> String -> String -> [(String,String)]
+hanoi :: Int -> String -> String -> String -> [(String,String)]
 
-hanoiLoesen 0 _ _ _ = []
-hanoiLoesen n a b c = hanoiLoesen (n-1) a c b ++ [(a,b)] ++ hanoiLoesen (n-1) c b a
+hanoi 0 _ _ _ = []
+hanoi n source dest temp = hanoi (n-1) source temp dest ++ [(source,dest)] ++ hanoi (n-1) temp dest source
