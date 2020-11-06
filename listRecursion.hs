610f3b8d2fed451fd6bf7429d61229abfb4b080c
Mino5531 <paffrath@yangnet.de>
2020-11-06 08:56:41 +0100

    Added insertionsort and comments

--- listRecursion.hs
+++ listRecursion.hs
@@ -1,6 +1,18 @@
+--Insertionsort
+insert :: Ord a => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y:ys) 
+    | x < y     = x:y:ys                
+    | otherwise = y:(insert x ys)
+isort :: Ord a => [a] -> [a]
+isort [] = []
+isort (x:xs) = insert x (isort xs)
+
+--Quicksort
 qsort []= []
 qsort (x:xs) = qsort [y | y<-xs, y<x] ++ [y | y<-xs, y==x] ++ [x] ++ qsort [y | y<-xs, y>x]
 
+--Mergesort
 merge :: Ord a => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
@@ -14,6 +26,7 @@ go xs = go (pairs xs)
 pairs (a:b:t) = merge a b : pairs t
 pairs t = t
 
+--Bubblesort
 bubbleSort uList = foldr swapTill [] uList
 
 swapTill x [] = [x]
